8a5ac08f7a54e2cf0c6bfa2e9391de2e54c27f84
Yasminebettaieb <ybettaieb93@gmail.com>
2021-07-06 17:05:01 +0100

    Revert "Revert "update""
    This reverts commit ce9a0703

--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -1,32 +1,16 @@
 package com.example.demo;
 
-import com.example.demo.controller.CategorieController;
-import com.example.demo.controller.ProduitController;
-import com.example.demo.entities.Categorie;
-import com.example.demo.entities.Produit;
-import com.example.demo.repository.CategorieRepository;
-import com.example.demo.repository.ProduitRepository;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.security.Timestamp;
 
 
 @SpringBootTest
 class Demo1ApplicationTests {
-    @Autowired
-    private ProduitRepository produitRepository;
-    @Autowired
-    private ProduitController produitController;
-    @Autowired
-    private CategorieRepository categorieRepository;
-    @Autowired
-    private CategorieController categorieController;
 
 
 
 
-    
 }
