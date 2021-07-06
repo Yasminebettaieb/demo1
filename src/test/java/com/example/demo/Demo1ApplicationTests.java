530a8351865fb754266d4b069bcf411fafcebb98
Yasminebettaieb <ybettaieb93@gmail.com>
2021-07-06 22:03:30 +0100

    Revert "Revert "update""
    This reverts commit ce9a0703

--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -3,19 +3,32 @@ package com.example.demo;
 import com.example.demo.controller.CategorieController;
 import com.example.demo.controller.ProduitController;
 import com.example.demo.entities.Categorie;
-import com.example.demo.entities.Produit;
 import com.example.demo.repository.CategorieRepository;
 import com.example.demo.repository.ProduitRepository;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-class Demo1ApplicationTests{
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
+@SpringBootTest
+class Demo1ApplicationTests {
+    @Autowired
+    private ProduitRepository produitRepository;
+    @Autowired
+    private ProduitController produitController;
+    @Autowired
+    private CategorieRepository categorieRepository;
+    @Autowired
+    private CategorieController categorieController;
+
+    @Test
+    void ajoutProducts() {
+        Categorie categorie = new Categorie("uniiiit", 4);
+        categorieController.ajoutCategorie(categorie);
+        assertTrue(categorieRepository.findById(categorie.getId()).isPresent());
+    }
 
 
 }
