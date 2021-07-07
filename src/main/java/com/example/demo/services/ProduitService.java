cf3de919054f46bb81722bbf42fa096d7b9c69f1
Yasminebettaieb <ybettaieb93@gmail.com>
2021-07-07 19:56:28 +0100

    Revert "Revert "update""
    This reverts commit ce9a0703

--- src/main/java/com/example/demo/services/ProduitService.java
+++ src/main/java/com/example/demo/services/ProduitService.java
@@ -2,7 +2,6 @@ package com.example.demo.services;
 import com.example.demo.entities.Produit;
 
 public interface ProduitService {
-    Produit saveProduit(Produit p);
 
     Produit updateProduit(Produit p, long id);
 
